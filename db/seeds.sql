3dd423079d0257e4a3411bab31d774a0d125f8f4
Sam Eaton <sameaton1989@gmail.com>
2018-12-04 21:31:21 -0500

    Initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Jonny Peltz's Butter-Basted Burger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Islamorada Larry's Alligator Burger", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Burger That Is Not A Burger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mongolian Yak Burger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hallucinated Burger", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hologram Burger", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hamburger", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Soylent Green Burger", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Simulacrum Burger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Theoretical Burger", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Aristotelian Ideal Of A Burger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Burger", true);
