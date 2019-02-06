0611fd005b4e9f941ccae6d40b95f53819234bc0
Sam Eaton <sameaton1989@gmail.com>
2019-02-06 00:01:05 -0500

    Readying for Heroku

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,20 +1,21 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  $(".change-sleep").on("click", function(event) {
+  $(".devour-burger").on("click", function(event) {
+    event.preventDefault();
     var id = $(this).data("id");
-    var newSleep = $(this).data("newsleep");
+    var devoured = $(this).data("devoured");
 
-    var newSleepState = {
+    var devouredState = {
         devoured: true
     };
 
     // Send the PUT request.
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
-      data: newSleepState
+      data: devouredState
     }).then(
       function() {
-        console.log("changed sleep to", newSleep);
+        console.log("changed sleep to", devoured);
         // Reload the page to get the updated list
         location.reload();
       }
@@ -42,18 +43,5 @@ $(function() {
     );
   });
 
-  $(".delete-cat").on("click", function(event) {
-    var id = $(this).data("id");
 
-    // Send the DELETE request.
-    $.ajax("/api/cats/" + id, {
-      type: "DELETE"
-    }).then(
-      function() {
-        console.log("deleted cat", id);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
 });
